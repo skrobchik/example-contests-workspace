4c3235e69e588e26e898902e526a908ae9473e04
Robert Serrano Kobylyansky <robertkobylyansky@gmail.com>
2024-03-31 15:23:31 +0100

    CSES Missing Numbers

---
+++ archive/2024/3/31.3.2024 - CSES Problem Set/missing_number.rs
@@ -0,0 +1,61 @@
+//{"name":"Missing Number","group":"CSES - CSES Problem Set","url":"https://cses.fi/problemset/task/1083","interactive":false,"timeLimit":1000,"tests":[{"input":"5\n2 3 1 5\n","output":"4\n"}],"testType":"single","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"MissingNumber"}}}
+
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &PreCalc) {
+    let n = input.read_int();
+    let mut nums = vec![false; (n+1) as usize];
+    for _ in 0..n-1 {
+        nums[input.read_int() as usize] = true;
+    }
+    for x in 1..=n {
+        if !nums[x as usize] {
+            out.print_line(x);
+            return;
+        }
+    }
+    unreachable!()
+}
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    let pre_calc = ();
+
+    #[allow(dead_code)]
+    enum TestType {
+        Single,
+        MultiNumber,
+        MultiEof,
+    }
+    let test_type = TestType::Single;
+    match test_type {
+        TestType::Single => solve(&mut input, &mut output, 1, &pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 0usize..t {
+                solve(&mut input, &mut output, i + 1, &pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &pre_calc);
+                i += 1;
+            }
+        }
+    }
+    output.flush();
+    input.skip_whitespace();
+    input.peek().is_none()
+}
+
+
+//START MAIN
+mod tester;
+
+fn main() {
+    tester::run_tests();
+}
+//END MAIN
